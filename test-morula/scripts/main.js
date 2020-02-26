ceecd15812da3c249ff82a1504901ad77e587607
kalithug <59684559+kalithug@users.noreply.github.com>
2020-02-26 20:44:38 +0200

    Add files via upload

---
+++ test-morula/scripts/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/stocks.jpg'){
+        myImage.setAttribute ('src', 'images/page.jpg');
+    } else {
+        myImage.setAttribute ('src', 'images/stocks.jpg')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name:');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is the shiznit, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool ' + storedName;
+}
+myButton.onclick = function(){
+    setUserName();
+}
